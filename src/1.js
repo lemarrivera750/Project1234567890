1c9422686a4e2531d4c56454475e1414816d219a
lemarrivera750 <lemarrivera750@gmail.com>
2025-03-05 19:12:56 +0000

    Add 1.js

---
+++ src/1.js
@@ -0,0 +1,9 @@
+// Function to generate a random number between 1 and 10
+function getRandomNumber() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+// Function to check if the number is even or odd
+function isEven(number) {
+    return (number % 2 === 0);
+}
